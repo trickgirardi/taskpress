bd4a910815ac10fa4ef030db41f19f44ce80c4d4
isTrick <patrickgirardidossantos@gmail.com>
2024-09-11 16:46:28 -0300

    almost finished

--- app/components/ListTasks.jsx
+++ app/components/ListTasks.jsx
@@ -17,16 +17,24 @@ export default function AddTask() {
         setTaskName(e.target.value);
     }; 
 
+    const toggleTaskCompletion = (index) => {
+        setTaskList(prevTasks => 
+            prevTasks.map((task, i) => 
+                i === index ? { ...task, completed: !task.completed } : task
+            )
+        );
+    };
+
     return (
-        <div>
-            <p>TASKS</p>
-            <ul>
+        <div className='flex flex-col items-center justify-around h-full max-w-xs bg-zinc-800 rounded-md p-3'>
+            <p className='m-3 text-white'>TASKS</p>
+            <ul className='m-3 p-3 w-full h-full border border-zinc-500 bg-zinc-900 rounded-md'>
                 {taskList.map((task, index) => (
-                    <li key={index}>{task}</li>
+                    <li onClick={() => toggleTaskCompletion(index)}  style={{ textDecoration: task.completed ? 'line-through' : 'none' }} className='hover:cursor-pointer text-white border-b border-b-zinc-500 mt-2 mb-2' key={index}>{taskName}</li>
                 ))}
             </ul>
-            <input type='text' value={taskName} onChange={handleChange} placeholder='Enter new task' />
-            <button onClick={handleAddTask}>Create task</button>
+            <input className='text-center m-3 bg-zinc-300 rounded-sm focus:outline-none' type='text' value={taskName} onChange={handleChange} placeholder='Enter new task' />
+            <button className='m-3 mt-0 rounded text-center bg-slate-700 p-3 text-slate-200' onClick={handleAddTask}>Create task</button>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,20 @@ import AddTask from './components/ListTasks.jsx'
 
 export default function Home() {
   return (
-    <>
-      <header className='flex flex-row flex-nowrap justify-between bg-zinc-800'>
-        <h1 className='p-7 text-white'>Nested Tasks</h1>
-        <div className='p-7 flex flex-row flex-nowrap gap-3 justify-between text-zinc-300'>
+    <div className='w-screen h-screen max-h-screen'>
+      <header className='flex flex-row flex-nowrap h-1/6 justify-around items-center bg-zinc-800'>
+        <h1 className='text-white'>Nested Tasks</h1>
+        <div className='flex flex-row flex-nowrap gap-3 justify-between text-zinc-300'>
           <a className='h:text-zinc-400' href="github.com">GitHub</a>
-          <p className='h:text-zinc-400'>Dark Mode</p>
+          <button className='h:text-zinc-400'>Dark Mode</button>
         </div>
       </header>
-      <main className='bg-zinc-950'>
-        <div className='bg-zinc-800'>
+      <main className='flex flex-row justify-center bg-zinc-950 h-4/6 p-2'>
           <AddTask />
-        </div>
       </main>
-    </>
+      <footer className='flex h-1/6 items-center justify-center bg-zinc-800'>
+        <p className='text-zinc-600'>Project under development.</p>
+      </footer>
+    </div>
   );
 }
