2b2d3f79f149fbdf3796f8455283a68409af279b
isTrick <patrickgirardidossantos@gmail.com>
2024-09-12 12:31:29 -0300

    added sectioned tasks

--- app/components/ListTasks.jsx
+++ app/components/ListTasks.jsx
@@ -25,12 +25,25 @@ export default function AddTask() {
         );
     };
 
+    const deleteTask = (index) => {
+        setTaskList(prevTasks => [
+            ...prevTasks.slice(0, index),
+            ...prevTasks.slice(index + 1),
+        ]);
+    };
+
     return (
-        <div className='flex flex-col items-center justify-around h-full max-w-xs bg-zinc-800 rounded-md p-3'>
+        <div className='flex flex-col items-center justify-around h-full w-m bg-zinc-800 rounded-md p-3'>
             <p className='m-3 text-white'>TASKS</p>
             <ul className='m-3 p-3 w-full h-full border border-zinc-500 bg-zinc-900 rounded-md'>
                 {taskList.map((task, index) => (
-                    <li onClick={() => toggleTaskCompletion(index)}  style={{ textDecoration: task.completed ? 'line-through' : 'none' }} className='hover:cursor-pointer text-white border-b border-b-zinc-500 mt-2 mb-2' key={index}>{task.name}</li>
+                    <li className='flex flex-row justify-between items-center text-white border-b border-b-zinc-500 mt-2 mb-2' key={index}>
+                        <p onClick={() => toggleTaskCompletion(index)}  style={{ textDecoration: task.completed ? 'line-through' : 'none' }} className='hover:cursor-pointer'>{task.name}</p>
+                        <div className=' flex flex-row gap-2'>
+                            <button className='text-zinc-500 text-center'>edit</button>
+                            <button onClick={() => deleteTask(index)} className=' text-center text-rose-400'>x</button>
+                        </div>
+                    </li>
                 ))}
             </ul>
             <input className='text-center m-3 bg-zinc-300 rounded-sm focus:outline-none' type='text' value={taskName} onChange={handleChange} placeholder='Enter new task' />
--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,24 @@ import AddTask from './components/ListTasks.jsx'
 
 export default function Home() {
   return (
-    <div className='w-screen h-screen max-h-screen'>
-      <header className='flex flex-row flex-nowrap h-1/6 justify-around items-center bg-zinc-800'>
+    <div className='w-screen h-screen max-h-screen overflow-hidden'>
+      <header className='flex flex-row flex-nowrap h-[10vh] justify-around items-center bg-zinc-800'>
         <h1 className='text-white'>Nested Tasks</h1>
         <div className='flex flex-row flex-nowrap gap-3 justify-between text-zinc-300'>
           <a className='h:text-zinc-400' href="github.com">GitHub</a>
           <button className='h:text-zinc-400'>Dark Mode</button>
         </div>
       </header>
-      <main className='flex flex-row justify-center bg-zinc-950 h-4/6 p-2'>
+      <main className='flex flex-row flex-wrap md:flex-nowrap gap-2 justify-center md:justify-normal bg-zinc-950 h-[80vh] p-2 overflow-y-auto scroll-smooth scrollbar'>
+          <AddTask />
+          <AddTask />
+          <AddTask />
+          <AddTask />
+          <AddTask />
+          <AddTask />
           <AddTask />
       </main>
-      <footer className='flex h-1/6 items-center justify-center bg-zinc-800'>
+      <footer className='flex h-[10vh] items-center justify-center bg-zinc-800'>
         <p className='text-zinc-600'>Project under development.</p>
       </footer>
     </div>
