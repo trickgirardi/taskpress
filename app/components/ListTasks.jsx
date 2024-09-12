b8b1306b8f5e0f9da2c11ada38db5fd95d50e760
isTrick <patrickgirardidossantos@gmail.com>
2024-09-12 10:43:40 -0300

    added toggle done tasks functionality

--- app/components/ListTasks.jsx
+++ app/components/ListTasks.jsx
@@ -8,7 +8,7 @@ export default function AddTask() {
     
     const handleAddTask = () => {
         if (taskName) {
-            setTaskList(prevTasks => [...prevTasks, taskName]);
+            setTaskList(prevTasks => [...prevTasks, { name: taskName, completed: false }]);
             setTaskName('');
         }
     }
@@ -30,7 +30,7 @@ export default function AddTask() {
             <p className='m-3 text-white'>TASKS</p>
             <ul className='m-3 p-3 w-full h-full border border-zinc-500 bg-zinc-900 rounded-md'>
                 {taskList.map((task, index) => (
-                    <li onClick={() => toggleTaskCompletion(index)}  style={{ textDecoration: task.completed ? 'line-through' : 'none' }} className='hover:cursor-pointer text-white border-b border-b-zinc-500 mt-2 mb-2' key={index}>{taskName}</li>
+                    <li onClick={() => toggleTaskCompletion(index)}  style={{ textDecoration: task.completed ? 'line-through' : 'none' }} className='hover:cursor-pointer text-white border-b border-b-zinc-500 mt-2 mb-2' key={index}>{task.name}</li>
                 ))}
             </ul>
             <input className='text-center m-3 bg-zinc-300 rounded-sm focus:outline-none' type='text' value={taskName} onChange={handleChange} placeholder='Enter new task' />
