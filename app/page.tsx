5fa61b8d1a842ce79d3bc1c1335028380c70de2b
isTrick <patrickgirardidossantos@gmail.com>
2024-09-17 13:31:49 -0300

    removed useless button and updated readme.md

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,9 @@ export default function Home() {
   return (
     <div className='w-screen h-screen max-h-screen overflow-hidden'>
       <header className='flex flex-row flex-nowrap h-[10vh] justify-around items-center bg-zinc-800'>
-        <h1 className='text-white'>Nested Tasks</h1>
+        <h1 className='text-white'>Taskpress</h1>
         <div className='flex flex-row flex-nowrap gap-3 justify-between text-zinc-300'>
-          <a className='h:text-zinc-400' href="github.com">GitHub</a>
-          <button className='h:text-zinc-400'>Dark Mode</button>
+          <a className='h:text-zinc-400' href="https://github.com/isTrick/taskpress" target='blank'>GitHub</a>
         </div>
       </header>
       <main className='flex flex-row gap-2 justify-center bg-zinc-950 h-[80vh] p-2 overflow-y-auto scroll-smooth scrollbar'>
